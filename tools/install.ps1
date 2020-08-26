0dface20bbbf64e14637be7412b5cc1f23aa13b2
Grant Curell <grantcurell@gmail.com>
2020-08-26 14:03:08 -0400

    Maybe....

--- tools/install.ps1
+++ tools/install.ps1
@@ -1,3 +1,4 @@
 choco install -y python3 chromedriver googlechrome
+cd "C:\ProgramData\chocolatey\lib\generatewebtraffic"
 Update-SessionEnvironment
 pip install PyYAML selenium validators dnspython
